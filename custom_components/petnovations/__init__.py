2c15a5b0cca72d0e0cc148ac4b2dad176f511e81
Andrew Benson <bensoae87@Andrews-MacBook-Pro.local>
2024-09-05 22:16:42 -0500

    update init file

--- custom_components/petnovations/__init__.py
+++ custom_components/petnovations/__init__.py
@@ -1,1 +1,15 @@
-"""The Petnovations integration."""
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_API_KEY, CONF_REFRESH_TOKEN
+
+DOMAIN = "petnovations"
+
+async def async_setup(hass, config):
+    return True
+
+async def async_setup_entry(hass, entry):
+    # Setup code here
+    return True
+
+async def async_unload_entry(hass, entry):
+    # Unloading code here
+    return True
